1798e65a887d85be7bd7f3066bee8281992d25d6
Dawid Frankiewicz <dawid.frankiewicz2001@gmail.com>
2022-11-08 04:52:31 +0100

    correct link

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,16 +1,16 @@
+import { Link } from 'react-router-dom';
+
 export default function NotFound() {
 	return (
 		<div className="container mx-auto text-center py-10">
 			<h1 className="text-6xl font-bold">Error 404</h1>
 			<h2 className="text-3xl font-bold">Page not found</h2>
-			<button
-				onClick={() => {
-					window.location.href = '/';
-				}}
-				className="bg-slate-600 text-white p-2 mt-10"
+			<Link
+				to="/"
+				className="bg-slate-600 text-white p-2 mt-10 rounded-md inline-block"
 			>
 				Home
-			</button>
+			</Link>
 		</div>
 	);
 }
