467f0b33edbe3d07c57946659b51795c2506c1e8
Mefiasty <freshminter@gmail.com>
2022-11-08 01:37:49 +0100

    deleating code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,27 +5,7 @@ function Footer() {
 	return (
 		<footer className="bg-gray-800 text-white text-center py-4 mt-20">
 			<div className="container mx-auto">
-				<ul>
-					© 2022 Copyright | Creators:
-					<li>
-						<a
-							href="https://github.com/DawidFrankiewicz"
-							className="hover:text-emerald-300"
-						>
-							<FontAwesomeIcon icon={faGithub} className="mr-2" />
-							Dawid Frankiewicz
-						</a>
-					</li>
-					<li>
-						<a
-							href="https://github.com/Mefiasty"
-							className="hover:text-emerald-300"
-						>
-							<FontAwesomeIcon icon={faGithub} className="mr-2" />
-							Mateusz Harbuz
-						</a>
-					</li>
-				</ul>
+				<ul>© 2022 Copyright | Creators:</ul>
 			</div>
 		</footer>
 	);
