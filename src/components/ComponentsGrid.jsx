09ea01ea0e722d4c5c5ff40a1f5ef960f0e96bca
Dawid Frankiewicz <dawid.frankiewicz2001@gmail.com>
2022-11-08 02:58:28 +0100

    components grid map

--- src/components/ComponentsGrid.jsx
+++ src/components/ComponentsGrid.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 
 function ComponentsGrid() {
 	// components data array
-	const componentsData = useState([
+	const [componentsData, setComponentsData] = useState([
 		{
 			name: 'Button',
 			thumbnail: 'https://i.imgur.com/4Q4Q4Q4.png',
@@ -30,72 +30,32 @@ function ComponentsGrid() {
 		}
 	]);
 
+	const openComponentRoute = (path) => {
+		console.log(path);
+	};
+
 	return (
 		<main className="grid md:grid-cols-3 2xl:grid-cols-4 gap-5 my-5">
-			<div className="group cursor-pointer">
-				<div className="bg-slate-300 rounded-t-lg h-64"></div>
-				<div className=" rounded-b-lg shadow-lg p-3">
-					<h3 className="text-react-blue font-bold text-2xl group-hover:text-cyan-900">
-						Component Name
-					</h3>
+			{componentsData.map((component, index) => (
+				<div
+					key={index}
+					className="group cursor-pointer"
+					onClick={() => openComponentRoute(component.path)}
+				>
+					<div className="bg-slate-300 rounded-t-lg h-64">
+						<img
+							src={component.thumbnail}
+							alt={component.name}
+							className="w-full h-full object-cover rounded-t-lg"
+						/>
+					</div>
+					<div className=" rounded-b-lg shadow-lg p-3">
+						<h3 className="text-react-blue font-bold text-2xl group-hover:text-cyan-900 transition-colors group-hover:drop-shadow-lg">
+							{component.name}
+						</h3>
+					</div>
 				</div>
-			</div>
-			<div className="group cursor-pointer">
-				<div className="bg-slate-300 rounded-t-lg h-64"></div>
-				<div className=" rounded-b-lg shadow-lg p-3">
-					<h3 className="text-react-blue font-bold text-2xl group-hover:text-cyan-900 ">
-						Component Name
-					</h3>
-				</div>
-			</div>
-			<div className="group cursor-pointer">
-				<div className="bg-slate-300 rounded-t-lg h-64"></div>
-				<div className=" rounded-b-lg shadow-lg p-3">
-					<h3 className="text-react-blue font-bold text-2xl group-hover:text-cyan-900">
-						Component Name
-					</h3>
-				</div>
-			</div>
-			<div className="group cursor-pointer">
-				<div className="bg-slate-300 rounded-t-lg h-64"></div>
-				<div className=" rounded-b-lg shadow-lg p-3">
-					<h3 className="text-react-blue font-bold text-2xl group-hover:text-cyan-900">
-						Component Name
-					</h3>
-				</div>
-			</div>
-			<div className="group cursor-pointer">
-				<div className="bg-slate-300 rounded-t-lg h-64"></div>
-				<div className=" rounded-b-lg shadow-lg p-3">
-					<h3 className="text-react-blue font-bold text-2xl group-hover:text-cyan-900">
-						Component Name
-					</h3>
-				</div>
-			</div>
-			<div className="group cursor-pointer">
-				<div className="bg-slate-300 rounded-t-lg h-64"></div>
-				<div className=" rounded-b-lg shadow-lg p-3">
-					<h3 className="text-react-blue font-bold text-2xl group-hover:text-cyan-900">
-						Component Name
-					</h3>
-				</div>
-			</div>
-			<div className="group cursor-pointer">
-				<div className="bg-slate-300 rounded-t-lg h-64"></div>
-				<div className=" rounded-b-lg shadow-lg p-3">
-					<h3 className="text-react-blue font-bold text-2xl group-hover:text-cyan-900">
-						Component Name
-					</h3>
-				</div>
-			</div>
-			<div className="group cursor-pointer">
-				<div className="bg-slate-300 rounded-t-lg h-64"></div>
-				<div className=" rounded-b-lg shadow-lg p-3">
-					<h3 className="text-react-blue font-bold text-2xl group-hover:text-cyan-900">
-						Component Name
-					</h3>
-				</div>
-			</div>
+			))}
 		</main>
 	);
 }
