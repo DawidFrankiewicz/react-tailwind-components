bd6bc4464780e926279e1a07e1f1a8c83cb29e1c
Dawid Frankiewicz <dawid.frankiewicz2001@gmail.com>
2022-11-08 02:08:41 +0100

    components data

--- src/components/ComponentsGrid.jsx
+++ src/components/ComponentsGrid.jsx
@@ -1,6 +1,35 @@
 import React from 'react';
+import { useState } from 'react';
 
-function App() {
+function ComponentsGrid() {
+	// components data array
+	const componentsData = useState([
+		{
+			name: 'Button',
+			thumbnail: 'https://i.imgur.com/4Q4Q4Q4.png',
+			description: 'Button component',
+			path: '/button'
+		},
+		{
+			name: 'Card',
+			thumbnail: 'https://i.imgur.com/4Q4QZ0M.png',
+			description: 'Card component',
+			path: '/card'
+		},
+		{
+			name: 'Input',
+			thumbnail: 'https://i.imgur.com/4Q4QZ0M.png',
+			description: 'Input component',
+			path: '/input'
+		},
+		{
+			name: 'Navbar',
+			thumbnail: 'https://i.imgur.com/4Q4QZ0M.png',
+			description: 'Navbar component',
+			path: '/navbar'
+		}
+	]);
+
 	return (
 		<main className="grid md:grid-cols-3 2xl:grid-cols-4 gap-5 my-5">
 			<div className="group cursor-pointer">
@@ -71,4 +100,4 @@ function App() {
 	);
 }
 
-export default App;
+export default ComponentsGrid;
