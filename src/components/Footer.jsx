0681788fe0dfd6c4e4ae8e0ef5f5901bf6fe215a
Dawid Frankiewicz <dawid.frankiewicz2001@gmail.com>
2022-11-08 01:29:41 +0100

    github icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 function Footer() {
 	return (
 		<footer className="bg-gray-800 text-white text-center py-4">
@@ -10,6 +12,7 @@ function Footer() {
 							href="https://github.com/DawidFrankiewicz"
 							className="hover:text-emerald-300"
 						>
+							<FontAwesomeIcon icon={faGithub} className="mr-2" />
 							Dawid Frankiewicz
 						</a>
 					</li>
@@ -18,6 +21,7 @@ function Footer() {
 							href="https://github.com/Mefiasty"
 							className="hover:text-emerald-300"
 						>
+							<FontAwesomeIcon icon={faGithub} className="mr-2" />
 							Mateusz Harbuz
 						</a>
 					</li>
