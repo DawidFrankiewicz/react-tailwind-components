94c9da6062c2123c6c94efee03d5919cf5d33bb0
Dawid Frankiewicz <dawid.frankiewicz2001@gmail.com>
2022-11-08 01:36:00 +0100

    margin top

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 function Footer() {
 	return (
-		<footer className="bg-gray-800 text-white text-center py-4">
+		<footer className="bg-gray-800 text-white text-center py-4 mt-20">
 			<div className="container mx-auto">
 				<ul>
 					© 2022 Copyright Creators:
