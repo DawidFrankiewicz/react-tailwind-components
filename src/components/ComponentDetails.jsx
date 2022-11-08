ce6f4cfd3504d15631a5a10a10c205cf0f2edc7a
Dawid Frankiewicz <dawid.frankiewicz2001@gmail.com>
2022-11-08 03:53:34 +0100

    component details

---
+++ src/components/ComponentDetails.jsx
@@ -0,0 +1,59 @@
+import { useState, useEffect } from 'react';
+export default function componentDetails() {
+	const [firstEffect, setFirstEffect] = useState(true);
+	const [componentData, setComponentData] = useState({});
+	const [componentsData, setComponentsData] = useState([
+		{
+			id: 1,
+			name: 'Button',
+			thumbnail: 'https://i.imgur.com/4Q4Q4Q4.png',
+			description: 'Button component'
+		},
+		{
+			id: 2,
+			name: 'Card',
+			thumbnail: 'https://i.imgur.com/4Q4QZ0M.png',
+			description: 'Card component'
+		},
+		{
+			id: 3,
+			name: 'Input',
+			thumbnail: 'https://i.imgur.com/4Q4QZ0M.png',
+			description: 'Input component'
+		},
+		{
+			id: 4,
+			name: 'Navbar',
+			thumbnail: 'https://i.imgur.com/4Q4QZ0M.png',
+			description: 'Navbar component'
+		}
+	]);
+
+	useEffect(() => {
+		if (firstEffect) {
+			const componentId = window.location.href.split('/').pop();
+			const component = componentsData.find(
+				(component) => component.id === parseInt(componentId)
+			);
+			setComponentData(component);
+			console.log('test');
+
+			setFirstEffect(false);
+		} else {
+			console.log('test2');
+		}
+	}, []);
+
+	return (
+		<div>
+			<h1 className="font-bold text-red-600">{componentData.name}</h1>
+			<img src={componentData.thumbnail} alt={componentData.name}></img>
+			<p className="font-bold text-blue-600">
+				Description:
+				<span className="font-thin text-gray-900">
+					{componentData.description}
+				</span>
+			</p>
+		</div>
+	);
+}
--- src/pages/ComponentDetails.jsx
+++ src/pages/ComponentDetails.jsx
@@ -1,56 +1,9 @@
-import { useState } from 'react';
+import ComponentDetails from '../components/ComponentDetails.jsx';
+
 export default function componentDetails() {
-	const [componentsData, setComponentsData] = useState([
-		{
-			id: 1,
-			name: 'Button',
-			thumbnail: 'https://i.imgur.com/4Q4Q4Q4.png',
-			description: 'Button component'
-		},
-		{
-			id: 2,
-			name: 'Card',
-			thumbnail: 'https://i.imgur.com/4Q4QZ0M.png',
-			description: 'Card component'
-		},
-		{
-			id: 3,
-			name: 'Input',
-			thumbnail: 'https://i.imgur.com/4Q4QZ0M.png',
-			description: 'Input component'
-		},
-		{
-			id: 4,
-			name: 'Navbar',
-			thumbnail: 'https://i.imgur.com/4Q4QZ0M.png',
-			description: 'Navbar component'
-		}
-	]);
-
-	// get the id from the url
-	const id = window.location.href.split('/').pop();
-
 	return (
-		<div>
-			<h1 className="font-bold text-red-600">
-				{componentsData[id].name}
-			</h1>
-			<img
-				src={componentsData[id].thumbnail}
-				alt={componentsData[id].name}
-			></img>
-			<p className="font-bold text-blue-600">
-				Description:
-				<span className="font-thin text-gray-900">
-					{componentsData[id].description}
-				</span>
-			</p>
-			<p className="font-bold text-blue-600">
-				Path:
-				<span className="font-thin text-gray-900">
-					{componentsData[id].path}
-				</span>
-			</p>
+		<div className="container mx-auto">
+			<ComponentDetails />
 		</div>
 	);
 }
