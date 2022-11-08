e99eea3e752f8941d558cc6618e270519b5a676c
Dawid Frankiewicz <dawid.frankiewicz2001@gmail.com>
2022-11-08 04:18:03 +0100

    display code

--- src/components/ComponentDetails.jsx
+++ src/components/ComponentDetails.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
 export default function componentDetails() {
 	const [firstEffect, setFirstEffect] = useState(true);
 	const [componentData, setComponentData] = useState({});
@@ -14,27 +16,31 @@ export default function componentDetails() {
 			id: 2,
 			name: 'Card',
 			thumbnail: 'https://i.imgur.com/4Q4QZ0M.png',
-			description: 'Card component'
+			description: 'Card component',
+			code: 'const [firstEffect, setFirstEffect] = useState(true);'
 		},
 		{
 			id: 3,
 			name: 'Input',
 			thumbnail: 'https://i.imgur.com/4Q4QZ0M.png',
-			description: 'Input component'
+			description: 'Input component',
+			code: 'const [firstEffect, setFirstEffect] = useState(true);'
 		},
 		{
 			id: 4,
 			name: 'Navbar',
 			thumbnail: 'https://i.imgur.com/4Q4QZ0M.png',
-			description: 'Navbar component'
+			description: 'Navbar component',
+			code: 'const [firstEffect, setFirstEffect] = useState(true);'
 		}
 	]);
 
+	const { id } = useParams();
+
 	useEffect(() => {
 		if (firstEffect) {
-			const componentId = window.location.href.split('/').pop();
 			const component = componentsData.find(
-				(component) => component.id === parseInt(componentId)
+				(component) => component.id === parseInt(id)
 			);
 			setComponentData(component);
 			console.log('test');
@@ -46,7 +52,7 @@ export default function componentDetails() {
 	}, []);
 
 	return (
-		<div>
+		<div id={componentData.id}>
 			<h1 className="font-bold text-red-600">{componentData.name}</h1>
 			<img src={componentData.thumbnail} alt={componentData.name}></img>
 			<p className="font-bold text-blue-600">
@@ -55,12 +61,12 @@ export default function componentDetails() {
 					{componentData.description}
 				</span>
 			</p>
-			<p>
-				<p>CODE CONTENT:</p>
+			<div>
+				CODE CONTENT:
 				<code className="bg-blue-50 p-2 rounded-lg ">
-					{componentsData.code}
+					{componentData.code}
 				</code>
-			</p>
+			</div>
 		</div>
 	);
 }
