80c346c20762a58ea4a38b0de20fd86413e71f45
Dawid Frankiewicz <dawid.frankiewicz2001@gmail.com>
2022-11-08 03:00:40 +0100

    require React eslint rule fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import Header from './components/Header.jsx';
 import ComponentsGrid from './components/ComponentsGrid.jsx';
 import Footer from './components/Footer.jsx';
