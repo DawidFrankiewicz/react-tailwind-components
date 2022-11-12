f52d12e6743eadebe2d10cfadca6c9e077281c2d
Dawid Frankiewicz <dawid.frankiewicz2001@gmail.com>
2022-11-13 00:23:40 +0100

    component code

---
+++ web/src/assets/components_code/button/Button.jsx
@@ -0,0 +1,10 @@
+function Button() {
+	return (
+		// button component
+		<button className="bg-react-blue text-white font-bold py-2 px-4 rounded">
+			Button
+		</button>
+	);
+}
+
+export default Button;
