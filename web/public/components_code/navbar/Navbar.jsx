048608314ec60a954e8ba34f6a357a47f3786eef
Mefiasty <freshminter@gmail.com>
2022-11-13 10:32:10 +0100

    adding more templates

---
+++ web/public/components_code/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+function Navbar() {
+	return (
+		<div className="navbar">
+			<div className="navbar__logo">
+				<img src="https://i.imgur.com/4l9qXQ1.png" alt="logo" />
+			</div>
+			<div className="navbar__links">
+				<a href="#">Home</a>
+				<a href="#">About</a>
+				<a href="#">Contact</a>
+			</div>
+		</div>
+	);
+}
---
+++ web/public/components_code/taskbar/Taskbar.jsx
@@ -0,0 +1,24 @@
+function Taskbar() {
+	return (
+		<div className="taskbar">
+			<div className="taskbar__start">
+				<img src="https://i.imgur.com/4l9qXQ1.png" alt="logo" />
+			</div>
+			<div className="taskbar__icons">
+				<a href="#">
+					<img src="https://i.imgur.com/4l9qXQ1.png" alt="logo" />
+				</a>
+				<a href="#">
+					<img src="https://i.imgur.com/4l9qXQ1.png" alt="logo" />
+				</a>
+				<a href="#">
+					<img src="https://i.imgur.com/4l9qXQ1.png" alt="logo" />
+				</a>
+				<a href="#">
+					<img src="https://i.imgur.com/4l9qXQ1.png" alt="logo" />
+				</a>
+			</div>
+		</div>
+	);
+}
+export default Taskbar;
