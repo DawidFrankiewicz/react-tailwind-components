a177879172543d3c5300dd410746c8920f2446c6
Dawid Frankiewicz <dawid.frankiewicz2001@gmail.com>
2022-11-14 20:01:55 +0100

    styling, error handling

--- web/src/components/ComponentDetailsDisplay.jsx
+++ web/src/components/ComponentDetailsDisplay.jsx
@@ -9,24 +9,42 @@ export default function componentDetailsDisplay() {
 	const [isLoading, setIsLoading] = useState(true);
 	const [componentData, setComponentData] = useState({});
 	const [componentCode, setComponentCode] = useState('');
-	// const [extraComponent, setExtraComponent] = useState('');
 	// TODO pass data from ComponentsGrid.jsx to ComponentDetails.jsx via router, still load from db if no data passe
 
+	// Get component id from url
 	const { id } = useParams();
+
 	useEffect(() => {
+		// Get component data from db by Id passed
 		getComponentsData(id).then((data) => {
 			// Not found error handling
 			if (!data) return (window.location.href = '/NotFound');
-			getCodeFromFile(data);
+			// If code path exists, load code from file
+			if (data.code_path) {
+				getCodeFromFile(data.code_path);
+			} else {
+				setComponentCode(null);
+			}
+			// Set component data
 			setComponentData(data);
+			// Stop loading animation, display component
 			setIsLoading(!isLoading);
 		});
 	}, []);
 
-	async function getCodeFromFile(data) {
-		const response = await fetch(`..${data.code_path}`);
-		const code = await response.text();
-		setComponentCode(code);
+	// Fetch code from file
+	async function getCodeFromFile(path) {
+		// fetcg file from path
+		const response = await fetch(`..${path}`);
+		if (response.ok) {
+			// Get code/text from response
+			const code = await response.text();
+			// Set code to state, displays on page
+			setComponentCode(code);
+		} else {
+			// handle error / displays nothing on page
+			setComponentCode(null);
+		}
 	}
 
 	return (
@@ -37,46 +55,59 @@ export default function componentDetailsDisplay() {
 				</div>
 			) : (
 				<main className="my-14">
-					<h1 className="font-bold text-red-600">
-						{componentData.name}
-					</h1>
-					<img
-						src={window.location.origin + componentData.thumbnail}
-						alt={componentData.name}
-					></img>
-					<p className="font-bold text-blue-600">
-						Description:
-						<span className="font-thin text-gray-900">
-							{componentData.description}
-						</span>
-					</p>
-					{componentCode ? (
-						<div>
-							<p>Source Code</p>
-							<SyntaxHighlighter
-								language="jsx"
-								className="my-5"
-								style={docco}
-							>
-								{componentCode}
-							</SyntaxHighlighter>
-							<CopyToClipboard text={componentCode}>
-								<button
-									type="copy"
-									className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-								>
-									Copy Code
-								</button>
-							</CopyToClipboard>
-						</div>
-					) : (
-						<div className="flex flex-col w-full justify-center items-center my-14">
-							<div className="animate-spin rounded-full h-32 w-32 border-b-4 border-react-blue"></div>
-							<p className="mt-4 text-lg font-bold animate-pulse">
-								Loading Code
+					<div className="lg:flex gap-8">
+						<div className="basis-1/3 grow">
+							<h1 className="font-bold text-4xl text-indigo-600 mb-4 border-b-4 border-b-gray-600 inline-block pr-12 pb-2">
+								{componentData.name}
+							</h1>
+							<p className="font-thin text-gray-700">
+								{componentData.description}
 							</p>
 						</div>
-					)}
+						<div>
+							<img
+								src={
+									window.location.origin +
+									componentData.thumbnail
+								}
+								alt={componentData.name}
+							/>
+						</div>
+					</div>
+					{/* If null, do not display code part */}
+					{componentCode !== null &&
+						// if code exists, display code part, else display loading animation
+						(componentCode ? (
+							<div className="my-4">
+								<div className="flex justify-between">
+									<h2 className="text-2xl font-bold">
+										Source Code
+									</h2>
+									<CopyToClipboard text={componentCode}>
+										<button
+											type="copy"
+											className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+										>
+											Copy
+										</button>
+									</CopyToClipboard>
+								</div>
+								<SyntaxHighlighter
+									language="jsx"
+									className="my-5"
+									style={docco}
+								>
+									{componentCode}
+								</SyntaxHighlighter>
+							</div>
+						) : (
+							<div className="flex flex-col w-full justify-center items-center my-14">
+								<div className="animate-spin rounded-full h-32 w-32 border-b-4 border-react-blue"></div>
+								<p className="mt-4 text-lg font-bold animate-pulse">
+									Loading Code
+								</p>
+							</div>
+						))}
 				</main>
 			)}
 		</div>
